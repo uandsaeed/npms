e44ec38ed40b8810634d07274e5fcf5cac342e76
Ariful Haque <ariful.haque@icarasia.com>
2018-07-29 15:33:05 +0800

    last login

---
+++ app/Listeners/SuccessfullLoginListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+//@todo add in queue
+class SuccessfullLoginListener
+{
+    /**
+     * Create the event listener.
+     *
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param Login|object $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        $event->user->last_login = date('Y-m-d H:i:s');
+        $event->user->save();
+    }
+}
